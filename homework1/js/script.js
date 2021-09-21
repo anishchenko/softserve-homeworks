2bf43c9b80deb2b660a9a7ce8c5c5ac2cce4042a
anishchenko <anishchenko.alexandr@gmail.com>
2021-09-21 22:44:51 +0300

    add task 1-3

--- homework1/js/script.js
+++ homework1/js/script.js
@@ -1,1 +1,11 @@
-alert('hello');
+// task2
+alert('anishchenko');
+
+//task3
+let e = 2.7;
+let pi = 3.14;
+console.log('e =', e, 'pi =', pi);
+e = pi;
+console.log('new value e =', e,', ', 'pi =', pi);
+
+//task4
